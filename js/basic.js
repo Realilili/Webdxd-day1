0907271685c23b845489fa433c1be5d90621ab70
Realilili <reali1210@gmail.com>
2016-03-05 12:53:31 -0800

    day 3
    add font-awesome

---
+++ js/basic.js
@@ -0,0 +1,62 @@
+var a = 1;
+// a is a integar
+var b = 0.5;
+//be is a float
+var c = "webdxd";
+//c is a string
+var d="1"
+console.log("this is my first javascript file");
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+var arr = [1,"learn",3,5,true];
+console.log(arr);
+console.log(arr[1]);
+var myQuote = "I learn";
+console.log("I "+ arr[1]);
+console.log(myQuote);
+a=8;
+a = a+b;
+//add a 1 to b 0.5 and put the result to a
+console.log (a);
+var bool = true;
+bool=!true;
+var isTrue =!bool;
+
+console.log(isTrue);
+arr= [isTrue, a, b, c];
+console.log(arr);
+a=11;
+b=2;
+b=a%b;
+console.log(b);
+var book={
+title: "Harry Potter",
+price: 29,
+description:"A bot who knows magic"
+}
+
+book.price = book.price+=2;
+//incremenet by 2
+book.price ++
+//incremenet by 1 -- decrement by 1
+console.log(book);
+console.log("Title: "+ book.title);
+console.log("price: "+ book.price);
+console.log("description: "+book.description);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
